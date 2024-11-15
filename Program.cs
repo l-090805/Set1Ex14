e48b8aede6596870bb27b5c0f4ef54748bc6f87a
falau <falau@LENOVO>
2024-11-15 21:03:01 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Set1Ex14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Intoduceti un numar: ");
+            int numar = int.Parse(Console.ReadLine());
+
+            if(Estepalindrom(numar))
+            {
+                Console.WriteLine("Numarul este palindrom");
+            }
+            else
+            {
+                Console.WriteLine("Numarul nu este palindrom");
+            }
+
+        }
+        static public bool Estepalindrom(int numar)
+        {
+
+            if (numar < 0 || numar % 10 == 0 && numar != 0)
+            {
+                return false;
+            }
+            else
+            {
+                int oglindit = 0;
+                while (numar > oglindit)
+                {
+                    oglindit = oglindit * 10 + numar % 10;
+                    numar /= 10;
+                }
+
+                return (oglindit == numar) || numar == oglindit / 10;
+            }
+        }
+    }
+}
